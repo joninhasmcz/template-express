877ec1292b9b0c3ba84266d4f4b937c3e505c71a
jonas.santos <jonas-asa-cor@hotmail.com>
2025-04-25 12:49:21 -0300

    feat(api) implement employee management API with CRUD operations and validation

---
+++ src/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+const empregadoRoutes = require('./routes/empregado');
+const connectionMongoDB = require('./config/db');
+const logger = require('./utils/logger');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'test') {
+    connectionMongoDB();
+}
+
+app.use('/api/empregados', empregadoRoutes);
+
+app.use((err, req, res, next) => {
+    logger.error(err.stack);
+    res.status(500).json({
+        erro: 'Erro interno do servidor',
+    })
+})
+
+app.listen(PORT, () => {
+    logger.info(`Servidor rodando na port ${PORT}`);
+})
--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
-const logger = require('../utils/logger');
+const mongoose = require("mongoose");
+const logger = require("../utils/logger");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 
 const connectionMongoDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-                usenewUrlParser: true,
-                useUnifiedTopologi: true,
-            });
-        logger.info('MongoDB Connected!');
-    } catch(error) {
-        logger.error('MongoDB No Connected: ' + error.message);
-        process.exit(1);
-    }
+  try {
+    console.log("PROCESS ENV: ", process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+    logger.info("MongoDB Connected!");
+  } catch (error) {
+    logger.error("MongoDB No Connected: " + error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectionMongoDB;
